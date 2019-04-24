a5741c3334431a27fc1ada433cbdeca3ce217f56
rahmor <rahmor@yahoo.com>
2019-04-24 12:00:52 -0400

    add context

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -1,44 +1,49 @@
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import CircleButton from '../CircleButton/CircleButton'
-import { countNotesForFolder } from '../notes-helpers'
-import './NoteListNav.css'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import NoteContext from '../NoteContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CircleButton from '../CircleButton/CircleButton';
+import { countNotesForFolder } from '../notes-helpers';
+import './NoteListNav.css';
 
 export default function NoteListNav(props) {
   return (
-    <div className='NoteListNav'>
-      <ul className='NoteListNav__list'>
-        {props.folders.map(folder =>
-          <li key={folder.id}>
-            <NavLink
-              className='NoteListNav__folder-link'
-              to={`/folder/${folder.id}`}
+    <NoteContext.Consumer>
+      {({ notes, folders }) => (
+        <div className='NoteListNav'>
+          <ul className='NoteListNav__list'>
+            {folders.map(folder => (
+              <li key={folder.id}>
+                <NavLink
+                  className='NoteListNav__folder-link'
+                  to={`/folder/${folder.id}`}
+                >
+                  <span className='NoteListNav__num-notes'>
+                    {countNotesForFolder(notes, folder.id)}
+                  </span>
+                  {folder.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <div className='NoteListNav__button-wrapper'>
+            <CircleButton
+              tag={Link}
+              to='/add-folder'
+              type='button'
+              className='NoteListNav__add-folder-button'
             >
-              <span className='NoteListNav__num-notes'>
-                {countNotesForFolder(props.notes, folder.id)}
-              </span>
-              {folder.name}
-            </NavLink>
-          </li>
-        )}
-      </ul>
-      <div className='NoteListNav__button-wrapper'>
-        <CircleButton
-          tag={Link}
-          to='/add-folder'
-          type='button'
-          className='NoteListNav__add-folder-button'
-        >
-          <FontAwesomeIcon icon='plus' />
-          <br />
-          Folder
-        </CircleButton>
-      </div>
-    </div>
-  )
+              <FontAwesomeIcon icon='plus' />
+              <br />
+              Folder
+            </CircleButton>
+          </div>
+        </div>
+      )}
+    </NoteContext.Consumer>
+  );
 }
 
 NoteListNav.defaultProps = {
   folders: []
-}
+};
